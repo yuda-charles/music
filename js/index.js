9cb647ad89de4398f115b029c81297d61deebcf6
yuda-charles <52034269+yuda-charles@users.noreply.github.com>
2019-12-10 18:10:40 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -71,7 +71,7 @@ function $(selector){
 
 function getMusicList(callback){
     var xhr = new XMLHttpRequest()
-    xhr.open('GET','../json/json.json',true)
+    xhr.open('GET','./json/json.json',true)
     xhr.onload = function(){
         if((xhr.status >=200 && xhr.status < 300) || xhr.status === 304){
             callback(JSON.parse(this.responseText))
